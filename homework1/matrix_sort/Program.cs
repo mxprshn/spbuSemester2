8fa474b24dd265b6d37e0ad7fa0ecda23994b56d
mxprshn <mxprshn@gmail.com>
2019-02-16 11:21:27 +0300

    First commit with C#

---
+++ homework1/matrix_sort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MatrixSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint height = 0;
+            uint width = 0;
+            Console.WriteLine("Enter the number of rows:");
+            Console.ReadLine(height);
+        }
+    }
+}
