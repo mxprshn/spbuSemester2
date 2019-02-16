51ea300d33eb566481a0a9d4432f41d0760850d8
mxprshn <mxprshn@gmail.com>
2019-02-16 23:48:16 +0300

    just QuickSort

---
+++ homework1/QuickSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { -1, -11, -11, 0, 75, 11, 134, -11, 13, 87, -11, 100, 35, 0 };
+
+            foreach (var i in array)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+            Sorting.Sort(ref array, 0, array.Length - 1);
+
+            foreach (var i in array)
+            {
+                Console.Write($"{i} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
