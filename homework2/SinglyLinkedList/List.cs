a2e62c3d830b556406f3db2f1e2453eead13148f
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2019-02-21 23:59:28 +0300

    Some drafts of singly linked list

---
+++ homework2/SinglyLinkedList/List.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SinglyLinkedList
+{
+    class List
+    {
+        private class Node
+        {
+            public int Value { get; }
+            public Node Next { get; set; }
+
+            public Node(int newValue, Node newNext)
+            {
+                Value = newValue;
+                Next = newNext;
+            }
+        }
+
+        private Node head = null;
+        public int Length { get; private set; } = 0;
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        private Node FindNode(int position)
+        {
+            var temp = head;
+
+            for (var i = Length - 1; i > position; --i)
+            {
+                temp = temp.Next;
+            }
+
+            return temp;
+        }
+
+        public void Insert(int newValue)
+        {
+            head = new Node(newValue, head);
+            ++Length;
+        }
+
+        public void InsertNext(int newValue, int previousPosition)
+        {
+
+        }
+
+        public void Remove()
+        {
+            head = head.Next;
+        }
+
+
+    }
+}
---
+++ homework2/SinglyLinkedList/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SinglyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
