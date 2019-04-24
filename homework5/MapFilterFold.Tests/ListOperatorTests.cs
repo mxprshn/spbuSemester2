b60fc76a762ae43d1d2a037bc0e79dba3cedf12f
mxprshn <mxprshn@gmail.com>
2019-04-24 23:27:16 +0300

    Bang! -- and code suddenly appeared.

---
+++ homework5/MapFilterFold.Tests/ListOperatorTests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MapFilterFold.Tests
+{
+    [TestFixture()]
+    public class ListOperatorTests
+    {
+        [TestCaseSource("MapTestSource")]
+        public void MapTest(List<int> testList, Func<int, int> testModifier, List<int> expectedResult)
+        {
+            var result = ListOperator.Map(testList, testModifier);
+            Assert.IsTrue(expectedResult.SequenceEqual(result));
+        }
+
+        [TestCaseSource("FilterTestSource")]
+        public void FilterTest(List<int> testList, Predicate<int> testFilter, List<int> expectedResult)
+        {
+            var result = ListOperator.Filter(testList, testFilter);
+            Assert.IsTrue(expectedResult.SequenceEqual(result));
+        }
+
+        [TestCaseSource("FoldTestSource")]
+        public void FoldTest(List<int> testList, int testAccumulated, Func<int, int, int> testAccumulator, int expectedAccumulated)
+        {
+            var result = ListOperator.Fold(testList, testAccumulated, testAccumulator);
+            Assert.AreEqual(expectedAccumulated, result);
+        }
+
+        private static object[] MapTestSource =
+        {
+            new object[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 },
+                new Func<int, int>(i => 5 * i),
+                new List<int> { 5, 10, 15, 20, 25 },
+            },
+
+            new object[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 },
+                new Func<int, int>(i => i),
+                new List<int> { 1, 2, 3, 4, 5 },
+            },
+
+            new object[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 },
+                new Func<int, int>(i => -2048),
+                new List<int> { -2048, -2048, -2048, -2048, -2048 },
+            },
+
+            new object[]
+            {
+                new List<int> { 2077 },
+                new Func<int, int>(i => i - 2000),
+                new List<int> { 77 },
+            },
+
+            new object[]
+            {
+                new List<int> {},
+                new Func<int, int>(i => i / 0),
+                new List<int> {},
+            }
+        };
+
+        private static object[] FilterTestSource =
+        {
+            new object[]
+            {
+                new List<int> { 1, 0, 3, 0, 5 },
+                new Predicate<int>(i => i != 0),
+                new List<int> { 1, 3, 5 },
+            },
+
+            new object[]
+            {
+                new List<int> { 100, 10000, 10, 1, 1 },
+                new Predicate<int>(i => i % 10 != 0),
+                new List<int> { 1, 1 },
+            },
+
+            new object[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 },
+                new Predicate<int>(i => i > 0),
+                new List<int> { 1, 2, 3, 4, 5 },
+            },
+
+            new object[]
+            {
+                new List<int> {},
+                new Predicate<int>(i => i % 0 == 0),
+                new List<int> {},
+            },
+
+            new object[]
+            {
+                new List<int> { 2077 },
+                new Predicate<int>(i => i != 2077),
+                new List<int> {},
+            },
+        };
+
+        private static object[] FoldTestSource =
+        {
+            new object[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 },
+                0,
+                new Func<int, int, int>((i, j) => i + j),
+                15,
+            },
+
+            new object[]
+            {
+                new List<int> {},
+                2077,
+                new Func<int, int, int>((i, j) => i + j),
+                2077,
+            },
+
+            new object[]
+            {
+                new List<int> { 2, 2, 2, 2, 2, 2, 2 },
+                2,
+                new Func<int, int, int>((i, j) => i * j),
+                256,
+            },
+
+            new object[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 },
+                2077,
+                new Func<int, int, int>((i, j) => 1024),
+                1024,
+            },
+
+            new object[]
+            {
+                new List<int> { 1, 2, 3, 4, 5 },
+                0,
+                new Func<int, int, int>((i, j) => i),
+                0,
+            },
+        };
+    }
+}
---
+++ homework5/MapFilterFold/ListOperator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MapFilterFold
+{
+    /// <summary>
+    /// Class containing methods operating with a list of integer numbers.
+    /// </summary>
+    public static class ListOperator
+    {
+        /// <summary>
+        /// Returns the new list containing elements of the source list modified by the given function.
+        /// </summary>
+        /// <param name="sourceList">List which elements are modified.</param>
+        /// <param name="modifier">Function defining the modification applied to the elements.</param>
+        /// <returns>New list which elements are modified elements of the source list.</returns>
+        public static List<int> Map(List<int> sourceList, Func<int, int> modifier)
+        {
+            var result = new List<int>();
+
+            foreach (var current in sourceList)
+            {
+                result.Add(modifier(current));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the new list containing only those elements of the source list which satisfy the predicate.
+        /// </summary>
+        /// <param name="sourceList">List which elements are filtered.</param>
+        /// <param name="filter">Predicate function defining the requirement for including an element to result list.</param>
+        /// <returns>New list which contains the elements satisfying the predicate.</returns>
+        public static List<int> Filter(List<int> sourceList, Predicate<int> filter)
+        {
+            var result = new List<int>();
+
+            foreach (var current in sourceList)
+            {
+                if (filter(current))
+                {
+                    result.Add(current);
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns an integer value got after recalculation the parameter for each element of the list.
+        /// </summary>
+        /// <param name="sourceList">List for which elements the value is recalculated.</param>
+        /// <param name="accumulated">Value to which the accumulator applied for the first time.</param>
+        /// <param name="accumulator">Function defining recalculation of the accumulated value for an element of list.</param>
+        /// <returns>Accumulated value.</returns>
+        public static int Fold(List<int> sourceList, int accumulated, Func<int, int, int> accumulator)
+        {
+            foreach (var current in sourceList)
+            {
+                accumulated = accumulator(accumulated, current);
+            }
+
+            return accumulated;
+        }
+    }
+}
