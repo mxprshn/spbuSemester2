62073d1182dc642aa4e2863885a5cc36c09fbd71
mxprshn <mxprshn@gmail.com>
2019-05-11 23:44:11 +0300

    Trying to fight copypast in generic tests

--- homework7/Set.Tests/SetTests.cs
+++ homework7/Set.Tests/SetTests.cs
@@ -7,44 +7,52 @@ namespace Set.Tests
     [TestFixture]
     public class SetTests
     {
-        private Set<int> testSet;
-
-        [SetUp]
-        public void SetUp()
+        public interface IAddTester
         {
-            testSet = new Set<int>();
+            void AddTest();
         }
 
-        [TestCaseSource("RemoveTestCases")]
-        public void RemoveTest(int[] valuesToAdd, int[] valuesToRemove)
+        public interface IRemoveTester
         {
-            foreach (var current in valuesToAdd)
-            {
-                testSet.Add(current);
-            }
-
-            foreach (var current in valuesToRemove)
-            {
-                Assert.IsTrue(testSet.Remove(current));
-                Assert.IsFalse(testSet.Contains(current));
-            }
-
-            Assert.AreEqual(0, testSet.Count);
+            void RemoveTest();
         }
 
-        private static object[] RemoveTestCases =
+        public class AddTester<T> : IAddTester where T : IComparable<T>
         {
-            new object[]
-            {
-                new int[] { 1, 2, 3, 4, 5 },
-                new int[] { 5, 4, 3, 2, 1 }
-            },
+            private Set<T> testSet = new Set<T>();
+            private T[] valuesToAdd;
+            private int expectedAmount;
 
-            new object[]
+            public AddTester(T[] valuesToAdd, int expectedAmount)
             {
-                new int[] { 0, 1, -1, 2, -2, 3, -3, -4, 4, 5, -5 },
-                new int[] { 0, 1, -1, 2, -2, 3, -3, -4, 4, 5, -5 }
+                this.valuesToAdd = valuesToAdd;
+                this.expectedAmount = expectedAmount;
             }
+
+            public void AddTest()
+            {
+                foreach (var current in valuesToAdd)
+                {
+                    testSet.Add(current);
+                    Assert.IsFalse(testSet.Add(current));
+                    Assert.IsTrue(testSet.Contains(current));
+                }
+
+                Assert.AreEqual(expectedAmount, testSet.Count);
+            }
+        }
+
+        [TestCaseSource("AddTestCases")]
+        public void RemoveTest(IAddTester tester) => tester.AddTest();
+
+        private static object[] AddTestCases =
+        {
+            new AddTester<int>(new int[] { 1, 2, 3, 4, 5 }, 5),
+            new AddTester<int>(new int[] { 0, 1, -1, 2, -2, 3, -3, -4, 4, 5, -5 }, 11),
+            new AddTester<int>(new int[] { 1, 1, 1, 1, 1 }, 1),
+            new AddTester<int>(new int[] { 1, 2, 1, 2, 1 }, 2),
+            new AddTester<string>(new string[] { "hello", "world", "Hello", "World", "" }, 5),
+            new AddTester<string>(new string[] { "January", "February", "March", "February", "January" }, 3)
         };
 
 
